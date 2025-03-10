3e41c9890c0b4e54ae85301669362f18fbedaa0e
paulinaheine <paulina.heine@uni-bielefeld.de>
2025-03-10 12:51:34 +0100

    test

---
+++ echte_strömung.py
@@ -0,0 +1,27 @@
+
+
+
+import matplotlib
+matplotlib.use('TkAgg')
+from datetime import datetime, timedelta
+import numpy as np
+from opendrift.readers.reader_constant_2d import Reader
+from opendrift.models.oceandrift import OceanDrift
+from opendrift.readers import reader_netCDF_CF_generic
+
+lon, lat = np.meshgrid(np.linspace(2,6,30), np.linspace(59,62,30))
+lon0 = 4
+lat0 = 60.5
+
+lon = np.linspace(0,5,30)
+lat = np.linspace(59,62,30)
+
+r = reader_netCDF_CF_generic.Reader(
+    'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
+
+o = OceanDrift(loglevel=20)
+o.set_config('environment:fallback:land_binary_mask', 0)
+o.add_reader(r)
+o.seed_elements(lon=3, lat=60.5, number=1000, radius=30000, time=datetime(2024, 6, 15, 1, 11, 11 ))
+o.run(duration=timedelta(hours=11))
+o.animation(fast=True)
