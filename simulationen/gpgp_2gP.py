358db8668256f00b28e262ca913ab5e749eed4c1
paulinaheine <paulina.heine@uni-bielefeld.de>
2025-03-18 12:02:55 +0100

    sort data etc.

---
+++ simulationen/gpgp_2gP.py
@@ -0,0 +1,52 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from datetime import datetime, timedelta
+import xarray as xr
+from opendrift.readers.reader_netCDF_CF_generic import Reader
+from opendrift.models.oceandrift import OceanDrift
+from Modelle.Plastic_Model_Custom import PlastElement, PlastDrift_M
+
+# Datenpfad
+data_path = '/Users/paulinaheine/Master Business Analytics/Masterarbeit/Technisches/TOPTC/data/currency_data/current_june2024'
+
+# Laden des Datensatzes mit xarray
+ds = xr.open_dataset(data_path)
+print(ds)
+
+# Einrichten des OceanDrift-Modells
+o = PlastDrift(loglevel=20)
+o2 = PlastDrift(loglevel=20)
+
+r = Reader(data_path)
+
+o.add_reader(r)
+o.set_config('environment:fallback:land_binary_mask', 0)
+
+o2.add_reader(r)
+o2.set_config('environment:fallback:land_binary_mask', 0)
+
+# Sicherstellen, dass start_time ein datetime-Objekt ist
+start_time = ds.time.values[0]
+if not isinstance(start_time, datetime):
+    start_time = datetime.utcfromtimestamp(start_time.astype(int) * 1e-9)
+
+# Setzen der Partikel
+mid_latitude = ds.latitude[int(len(ds.latitude) / 2)]
+mid_longitude = ds.longitude[int(len(ds.longitude) / 2)]
+depth = ds.depth.values[0]  # Die einzige verfügbare Tiefe
+
+o.seed_elements(lon=mid_longitude, lat=mid_latitude, number=20, radius=30000, z=-depth, time=start_time)
+
+# Simulation durchführen
+o.run(duration=timedelta(hours=190))
+
+#o2.seed_elements(lon=mid_longitude, lat=mid_latitude, number=10, radius=30000, z=-depth, time=start_time)
+
+o2.seed_elements(lon=mid_longitude, lat=mid_latitude, number=20, radius=30000,time=start_time, z=-depth, weight=0.025, size=0.0025, density=250)
+
+
+# Simulation durchführen
+o2.run(duration=timedelta(hours=190))
+
+# Ergebnis visualisieren
+o.animation(fast = True, compare = o2)
